feb1d4150ae0ce0f89a087ba3ba3cadeb831e37e
taras <tark000@ukr.net>
2015-10-28 10:35:58 +0200

    add slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function ($) {
+        var options = {
+            $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$,
+                $ChanceToShow: 2
+            }
+        };
+        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    });
