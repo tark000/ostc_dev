e92daa59a258e0f583c7c4f767f3d3a58c57d4e8
taras <tark000@ukr.net>
2015-10-30 01:43:14 +0200

    calendar 01

--- js/script.js
+++ js/script.js
@@ -18,11 +18,23 @@ $(document).ready(function ($) {
 			};
 
 		var nslider = new NinjaSlider(nsOptions);
+
+		calendar.setOptions({display_week_numbers: false});
+calendar.setOptions({weekbox: false});
     });
 
 var calendar = $("#calendar").calendar(
     {	
-    	language: 'bg-BG',
         tmpl_path: "tmpls/",
-        events_source: function () { return []; }
+        events_source: function () { return [
+        		{
+		            "id": 293,
+		            "title": 'Title: "The name of training."',
+		            "url": "http://example.com",
+		            "class": "event-important",
+		            "start": 1446157104493, // Milliseconds
+		            "end": 1446157109493 // Milliseconds
+		        }
+
+        	]; }
     });         
