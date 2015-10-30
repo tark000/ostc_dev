9c4955caacb6187519f37c0627d26ed87e2f900d
taras <tark000@ukr.net>
2015-10-30 02:09:22 +0200

    calendar work

--- js/script.js
+++ js/script.js
@@ -17,24 +17,33 @@ $(document).ready(function ($) {
 			    license: "mylicense"
 			};
 
-		var nslider = new NinjaSlider(nsOptions);
+		// var nslider = new NinjaSlider(nsOptions);
 
-		calendar.setOptions({display_week_numbers: false});
-calendar.setOptions({weekbox: false});
+		$(".responsive-calendar").responsiveCalendar({
+	          time: '2015-10',
+	          events: {
+	            "2015-10-30": {"url": "http://w3widgets.com/responsive-slider", "title": "test"},
+	            "2015-10-26": {"url": "http://w3widgets.com", "title": "test"}, 
+	            "2015-11-03":{"title": "test"}, 
+	            "2015-11-12": {"title": "test"}
+	        	}
+	        });
+
+
     });
 
-var calendar = $("#calendar").calendar(
-    {	
-        tmpl_path: "tmpls/",
-        events_source: function () { return [
-        		{
-		            "id": 293,
-		            "title": 'Title: "The name of training."',
-		            "url": "http://example.com",
-		            "class": "event-important",
-		            "start": 1446157104493, // Milliseconds
-		            "end": 1446157109493 // Milliseconds
-		        }
+// var calendar = $("#calendar").calendar(
+//     {	
+//         tmpl_path: "tmpls/",
+//         events_source: function () { return [
+//         		{
+// 		            "id": 293,
+// 		            "title": 'Title: "The name of training."',
+// 		            "url": "http://example.com",
+// 		            "class": "event-important",
+// 		            "start": 1446157104493, // Milliseconds
+// 		            "end": 1446157109493 // Milliseconds
+// 		        }
 
-        	]; }
-    });         
+//         	]; }
+//     });         
